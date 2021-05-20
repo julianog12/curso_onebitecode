ab101c5374e53028a58c9beddda43bbfb05c0dac
Juliano <julianog12@hotmail.com>
2021-05-20 14:51:51 -0300

    Novas aulas

---
+++ Aula10/script.js
@@ -0,0 +1,9 @@
+let nameOldPeople = prompt("Informe o nome da pessoa mais velha: ")
+let ageOldPeople = prompt("Informe a idade da pessoa mais velha: ")
+
+let nameNewPeople = prompt("Informe o nome da pessoa mais nova:")
+let ageNewPeople = prompt("Informe a idade da pessoa mais nova: ")
+
+let diferIdade = ageOldPeople - ageNewPeople
+
+alert("O "+nameOldPeople+" tem "+ageOldPeople+"anos\n"+nameNewPeople+" tem "+ageNewPeople+"\ne asua difereça de idade é de "+diferIdade+" anos")
