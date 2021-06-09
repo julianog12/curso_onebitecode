f50e294a88ef6d9776d86e4f62570e458de8d74f
Juliano <julianog12@hotmail.com>
2021-06-09 14:06:54 -0300

    eventos

---
+++ eventos_html/js/evento.js
@@ -0,0 +1,19 @@
+function clickMe(){
+  console.log("Fui Clicado")
+}
+
+function focusMe(){
+  console.log("Fui Focado")
+}
+
+function onblurMe(){
+  console.log("Fui desfocado")
+}
+
+function onchangeMe(){
+  console.log("Fui alterado")
+}
+
+function overMe(){
+  console.log("Passou o mouse sobre mim")
+}
